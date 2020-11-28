4d26664298c9fddc8b5bfc08373b366099470c40
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-11-27 20:11:30 -0500

    Fix IsExternalInit bug with Net Standard

--- src/StateR.Blazor.Experiments/IsExternalInit.cs
+++ src/StateR.Blazor.Experiments/IsExternalInit.cs
@@ -1,8 +1,6 @@
-﻿#if NETSTANDARD
-// Fix the error CS0518: Predefined type 'System.Runtime.CompilerServices.IsExternalInit' is not defined or imported
+﻿// Fix the error CS0518: Predefined type 'System.Runtime.CompilerServices.IsExternalInit' is not defined or imported
 // Allows to use C# 9 init-only properties/records into NETSTANDARD libs
 namespace System.Runtime.CompilerServices
 {
     public class IsExternalInit { }
 }
-#endif
