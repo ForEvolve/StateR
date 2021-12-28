def8e045a49499071c1e531ff0fdbc3ccea6ac42
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-12-28 10:25:09 -0500

    Add Dispatch components

---
+++ src/StateR.Blazor.Experiments/Components/DispatchActionFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+
+namespace StateR.Blazor.Components;
+
+public class DispatchActionFactory<TAction> : StatorComponentBase
+    where TAction : IAction
+{
+    [Parameter]
+    public Func<ValueTask<TAction>>? ActionFactory { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        GuardAgainstNullAction();
+        var action = await ActionFactory.Invoke();
+        await DispatchAsync(action);
+        await base.OnInitializedAsync();
+    }
+
+    [MemberNotNull(nameof(ActionFactory))]
+    private void GuardAgainstNullAction()
+    {
+        ArgumentNullException.ThrowIfNull(ActionFactory, nameof(ActionFactory));
+    }
+}
